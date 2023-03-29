6779c55f811693b7a5164b0876ce3aa829476b90
Lucas Rose <107737308+Lucas-Rose@users.noreply.github.com>
2023-03-29 16:21:15 +1100

    Implement Projectile Dispenser

---
+++ Assets/Scripts/DispenserController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DispenserController : MonoBehaviour
+{
+    [SerializeField] private GameObject projectilePrefab;
+    [SerializeField] private float dispenseTime;
+    [SerializeField] private float projectileSpeed;
+    private float currentTime;
+
+    private BoxCollider col; //used to determine the size of the area in which projectiles can spawn
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = dispenseTime;
+        col = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= Time.deltaTime;
+        if(currentTime <= 0)
+        {
+            Debug.Log("Dispensing");
+            SpawnProjectile();
+            currentTime = dispenseTime;
+        }
+    }
+
+    public void SpawnProjectile()
+    {
+        float xPos = Random.Range(transform.position.x - (col.size.x / 2), transform.position.x + (col.size.x / 2));
+        float yPos = Random.Range(transform.position.y - (col.size.y / 2), transform.position.y + (col.size.y / 2));
+        Quaternion randomRotation = Random.rotation;
+        GameObject newProj = Instantiate(projectilePrefab, new Vector3(xPos, yPos, transform.position.z), randomRotation);
+        newProj.GetComponent<Rigidbody>().velocity = -Vector3.forward * projectileSpeed;
+    }
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using StarterAssets;
+
+
+
+public class PlayerHealth : MonoBehaviour
+{
+    //public ThirdPersonController controller; //This one should be replaced with whatever player object
+    public float maxHealth;
+    public float currentHealth;
+    public float defence; //used for variable health scaling
+    private float defenceAsPercentage;
+    public float shakeScale;
+    [HideInInspector]
+    public bool invulnerable = false;
+    //public float deathDuration = 4;
+
+    public void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void Update()
+    {
+        defenceAsPercentage = (100 / (100 + defence));
+    }
+
+    public void takeDamage(float damage)
+    {
+        Debug.Log(invulnerable);
+        if (invulnerable) { Debug.Log("Damage Taken"); return; }
+
+        //AkSoundEngine.PostEvent("Player_Damage", gameObject);
+        //CameraListener.instance.CameraShake(damage * shakeScale, 0.25f);
+        //UIDamageIn.instance.DamageVis();
+
+        damage *= (100/(100 + defence));
+        damage = Mathf.Clamp(damage, 0f, Mathf.Infinity);
+        currentHealth -= damage;
+
+        switch (Alive()) 
+        {
+            case false:
+                StartCoroutine(Death());
+                break;
+            case true:
+                //EventManager.instance.OnHealthChange?.Invoke((currentHealth/maxHealth) * 100);
+                //EventManager.instance.OnTakeDamage?.Invoke(damage);
+                break;
+        }
+    }
+
+    public bool Alive()
+    {
+        return currentHealth > 0;
+    }
+
+    public void healDamage(float healing)
+    {
+        currentHealth += healing;
+        currentHealth = Mathf.Clamp(currentHealth, 0f, maxHealth);
+        //EventManager.instance.OnHealthChange?.Invoke((currentHealth / maxHealth) * 100);
+    }
+    
+    public IEnumerator Death()
+    {
+        invulnerable = true;
+        yield return new WaitForSeconds(1); //placeholder.
+
+        //do events on death.
+
+
+        //controller._animator.SetTrigger("Death");
+
+        //yield return new WaitForSeconds(controller._animator.GetCurrentAnimatorStateInfo(0).length * 0.5f);
+        //yield return AnnouncementHandler.instance.Announcement("You Died.", deathDuration);
+
+        //HealthbarManager.instance.ClearBoss();
+        //GameManager.instance.LoadDelegate(GameManager.instance.OnDeath()); //to be moved to whatever is handling health
+    }
+
+    public float GetPlayerHealth()
+    {
+        return currentHealth;
+    }
+}    

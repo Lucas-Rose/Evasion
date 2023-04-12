018e6b836421c6ff697b32164434b7e41f0400a6
Lucas Rose <lucas.rose@students.mq.edu.au>
2023-04-12 12:25:53 +1000

    Basic Beach Implementation

---
+++ Assets/Scripts/CannonTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonTrigger : ScriptableObject
+{
+        [SerializeField] private float time;
+        [SerializeField] private float speed;
+        [SerializeField] private int cannon;
+        public CannonTrigger init(float time, float speed, int cannon)
+        {
+            this.time = time;
+            this.speed = speed;
+            this.cannon = cannon;
+        return this;
+        }
+        public float getTime()
+        {
+            return time;
+        }
+        public float getSpeed()
+        {
+            return speed;
+        }
+        public int getCannon()
+        {
+            return cannon;
+        }
+}
---
+++ Assets/Scripts/PirateManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PirateManager : MonoBehaviour
+{
+    [SerializeField] private GameObject cBallPrefab;
+    [SerializeField] private GameObject player;
+
+    [SerializeField] private GameObject cannons;
+    [SerializeField] private List<GameObject> cannonObjects;
+    [SerializeField] private List<CannonTrigger> cannonTriggers;
+    [SerializeField] private List<float> triggerTimes = new List<float>();
+
+    private float timePassed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < cannons.transform.childCount; i++)
+        {
+            cannonObjects.Add(cannons.transform.GetChild(i).gameObject);
+        }
+        // Add cannon sequence here
+        cannonTriggers.Add(ScriptableObject.CreateInstance<CannonTrigger>().init(3f, 50f, 3));
+        cannonTriggers.Add(ScriptableObject.CreateInstance<CannonTrigger>().init(6f, 50f, 7));
+        cannonTriggers.Add(ScriptableObject.CreateInstance<CannonTrigger>().init(9f, 50f, 1));
+        cannonTriggers.Add(ScriptableObject.CreateInstance<CannonTrigger>().init(12f, 50f, 10));
+        cannonTriggers.Add(ScriptableObject.CreateInstance<CannonTrigger>().init(15f, 50f, 13));
+        cannonTriggers.Add(ScriptableObject.CreateInstance<CannonTrigger>().init(18f, 50f, 16));
+
+        for (int i = 0; i < cannonTriggers.Count; i++)
+        {
+            triggerTimes.Add(cannonTriggers[i].getTime());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(timePassed);
+        timePassed += Time.deltaTime;
+        if (timePassed >= triggerTimes[0])
+        {
+            GameObject newBall = Instantiate(cBallPrefab, cannonObjects[cannonTriggers[0].getCannon()].transform);
+            Rigidbody rb = newBall.AddComponent<Rigidbody>();
+            Vector3 direction = (player.transform.position - newBall.transform.position).normalized;
+            rb.velocity = direction * cannonTriggers[0].getSpeed();
+            rb.useGravity = false;
+            Debug.Log("spawned ball");
+            triggerTimes.RemoveAt(0);
+            cannonTriggers.RemoveAt(0);
+        }
+    }
+}

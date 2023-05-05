49355bccb90c8fb463c066c0db139ee8da95b858
Jason Yiu <jason-zi-xin.yiu@students.mq.edu.au>
2023-05-05 19:21:38 +1000

    Randomized Midpoint

--- Assets/Scripts/BCurveProjectile.cs
+++ Assets/Scripts/BCurveProjectile.cs
@@ -7,51 +7,89 @@ using UnityEngine;
 public class BCurveProjectile : MonoBehaviour
 {
     [SerializeField] private Vector3 origin;
-    [SerializeField] private Vector3 dest;
+    private Vector3 dest;
     [SerializeField] private float timeToDest = 2;
     private float curTime = 0;
     private Rigidbody rb;
     private float randomRange;
+    private bool init = false;
+    private Vector3 midpoint;
 
     //NOTE: Noticing a bit of issue with the frame rate dips affecting the calculation of the curve
     //It's less noticible when the if the curve is shallow or if the projectlile travels at slower speed
     void Awake()
-    {
+    {   
+        dest = new Vector3(0,0,10);
         rb = GetComponent<Rigidbody>();
         origin = transform.position;
-        //rb.angularVelocity = new Vector3(100f/Random.Range(1, 100), 100f/Random.Range(1, 100), 100f/Random.Range(1, 100));
+        rb.angularVelocity = new Vector3(100f/Random.Range(1, 100), 100f/Random.Range(1, 100), 100f/Random.Range(1, 100));
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        //initiate values if haven't
+        if (!init)
+        {
+            midpoint = RandomXYSign((origin + dest)/2, 3f, 10f);
+            init = true;
+            Debug.Log(midpoint);
+        }
     }
 
     //Fixedupdate for consistency of calculation
     void FixedUpdate()
     {
-        //Advance of current time
-        curTime += Time.fixedDeltaTime;
-        //After t = 1, stop following the curve
-        if(curTime < timeToDest)
-        {   
-            QuadraticBazierCurve(origin, dest);
+        if(init)
+        {
+            //Advance of current time
+            curTime += Time.fixedDeltaTime;
+
+            //After t = 1 (reaching destination), stop following the curve
+            if(curTime < timeToDest && init)
+            {   
+                QuadraticBazierCurve(origin,midpoint,dest);
+            }
         }
+
     }
 
-    public void QuadraticBazierCurve(Vector3 p0, Vector3 p2)
+    public void QuadraticBazierCurve(Vector3 p0, Vector3 p1, Vector3 p2)
     {
+        Debug.Log(p1);
         //t calculation
         float t = curTime/timeToDest;
 
-        //Randomize midpoint
-        Vector3 p1 = (p0 + p2)/2 + new Vector3(0, 10, 0);
-
-        //Lerp p0 ~ p1
         Vector3 lerp1 = Vector3.Lerp(p0, p1, t);
-
-        //Lerp p1 ~ p2
         Vector3 lerp2 = Vector3.Lerp(p1, p2, t);
-
-        //Lerp lerp1 ~ lerp2
         Vector3 targetP = Vector3.Lerp(lerp1,lerp2, t);
         
         //Moving the object to the next point in the curve
         rb.velocity =  (targetP - transform.position)/Time.fixedDeltaTime;
+    }
+
+
+    private Vector3 RandomXY(Vector3 point, float range)
+    {
+        return point = new Vector3(point.x + Random.Range(-range, range), point.y +Random.Range(-range, range), point.z);
+    }
+
+    //Randomize x,y of vector3 but in the form of a hollowed square
+    //Purpose: to make sure midpoints arcs aren't too small to be noticed while still being able to random in all directions
+    //Use positive numbers only
+    private Vector3 RandomXYSign(Vector3 point, float min, float max)
+    {
+        return point = new Vector3(point.x + RandomSign() * Random.Range(min, max), point.y + RandomSign() * Random.Range(min, max), point.z);
+    }
+
+
+    private float RandomSign()
+    {
+        int randomSign = Random.Range(0,2);
+        float sign = ((float)randomSign - 0.5f) *2;
+        return sign;
     }
 }

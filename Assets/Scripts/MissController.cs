3825bcb289dcad7f129d9bf77c122289762ca922
Jason Yiu <jason-zi-xin.yiu@students.mq.edu.au>
2023-06-14 18:08:32 +1000

    Pitch + Volume randomizer on missManager

--- Assets/Scripts/MissController.cs
+++ Assets/Scripts/MissController.cs
@@ -5,14 +5,49 @@ using UnityEngine;
 public class MissController : MonoBehaviour
 {
     [Header("Audio")]
-    [SerializeField] private List<AudioSource> missSounds;
+    //[SerializeField] private List<AudioSource> missSounds;
+    [SerializeField] private AudioClip[] clips;
+    [SerializeField] private AudioSource audSource;
 
+    [Header("Randomizer Values")]
+    [Range(0,0.49f)][SerializeField] private float minVolumePercentage = 0.3f;
+    [Range(0.5f,1f)][SerializeField] private float maxVolumePercentage = 0.7f;
+    [Range(0,0.99f)][SerializeField] private float minPitchPercentage = 0.3f;
+    [Range(1,2f)][SerializeField] private float maxPitchPercentage = 1;
+
+
+    void Start()
+    {
+        audSource = GetComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("projectile"))
         {
-            missSounds[Random.Range(0, missSounds.Count)].Play();
+            //missSounds[Random.Range(0, missSounds.Count)].Play();
+
+            PlayRandomClip();
         }
+    }
+
+    void Update()
+    {
+        //Uncommet below section for testing
+        // if(Input.GetKeyDown(KeyCode.P))
+        // {
+        //     PlayRandomClip();
+        // }
+    }
+
+
+    //Play sound at random pitch and volume
+    private void PlayRandomClip()
+    {
+        audSource.clip = clips[Random.Range(0, clips.Length)];
+        audSource.volume = Random.Range(minVolumePercentage, maxVolumePercentage);
+        audSource.pitch = Random.Range(minPitchPercentage,maxPitchPercentage);
+
+        audSource.PlayOneShot(audSource.clip);
     }
 }
